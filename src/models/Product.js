b3925a3667be1183838a21a6a65ab17d603b84bc
Jindrg <jindrg@gmail.com>
2022-09-24 12:56:19 -0300

    Agregando Model Product y Local

---
+++ src/models/Product.js
@@ -0,0 +1,23 @@
+const { model } = require('mongoose');
+class Product {
+
+    constructor(barCode, nomCat,idSuc,idSupplier, marca, description,stock, salePrice, purchasePrice ) {
+        this.barCode = barCode;
+        this.nomCat = nomCat;
+        this.idSuc = idSuc;
+        this.idSupplier = idSupplier;
+        this.marca = marca;
+        this.description = description;
+        this.stock = stock;
+        this.salePrice = salePrice;
+        this.purchasePrice = purchasePrice
+        
+    }
+    getAll() {
+        return `Product[barCode:${this.barCode}, nomCat:${this.nomCat}, idSuc:${this.idSuc}, idSupplier:${this.idSupplier}, marca:${this.marca}, description:${this.description}, stock:${this.stock}, salePrice:${this.salePrice}, purchasePrice:${this.purchasePrice}]`;
+    }
+
+}
+
+//module.exports = Product;
+module.exports = Product
