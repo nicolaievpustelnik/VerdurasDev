2d3dddbfb0f91993f3319304586a3ae7104c0a7d
Nicolaiev Brito Pustelnik <nicolaievbrito@gmail.com>
2022-09-26 23:18:03 -0300

    Se agrega modificaciones

--- src/models/Sucursal.js
+++ src/models/Sucursal.js
@@ -1,16 +1,26 @@
+const { model } = require('mongoose');
+
 const Product = require("./Product");
 const Usuario = require("./User");
 
+const sucursalSchema = require("./schemas/SucursalSchema");
+
 class Sucursal {
 
-
     constructor(idSuc, nomSuc, ubicacion) {
-        this.idSuc = idLocal;
+        this.idSuc = idSuc;
         this.nomSuc = nomSuc;
         this.ubicacion = ubicacion;
-        let user = [];
-        let product = [];
+        this.users = [];
+        this.products = [];
+    }
+
+    setProduct(productAux) {
+        let p = new Product(productAux)
+        this.products.push(p)
     }
 
 }
-module.exports = Sucursal;
+
+sucursalSchema.loadClass(Sucursal);
+module.exports = model('Sucursal', sucursalSchema);
