13cf7f33bf6c603baa6ed5c44b4ef1509368922d
Deby Landa <92892665+Deby93@users.noreply.github.com>
2022-09-27 19:33:15 -0300

    creacion de reporte

--- src/models/Admin.js
+++ src/models/Admin.js
@@ -1,20 +1,18 @@
-const { model } = require('mongoose');
+const { model } = require("mongoose");
 
-const User = require('./User');
-const userSchema = require('./UserSchema');
-
+const User = require("./User");
+const userSchema = require("./UserSchema");
 
 class Admin extends User {
+  constructor(firstName, lastName, email, password, sucursal) {
+    super(firstName, lastName, email, password, sucursal);
+    this.sucursal = sucursal;
+  }
 
-    constructor(firstName, lastName, email, password, sucursal) {
-        super(firstName, lastName, email, password, sucursal)
-        this.sucursal = sucursal
-    }
-
-    getAll() {
-        return `Admin[firstName:${this.firstName}, lastName:${this.lastName}, lastName:${this.email}, lastName:${this.password}, sucursal:${this.sucursal}]`;
-    }
+  getAll() {
+    return `Admin[firstName:${this.firstName}, lastName:${this.lastName}, email:${this.email}, password:${this.password}, sucursal:${this.sucursal}]`;
+  }
 }
 
 userSchema.loadClass(Admin);
-module.exports = model('Admin', userSchema);
+module.exports = model("Admin", userSchema);
