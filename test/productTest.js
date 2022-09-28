4c713b995e9dc3e02a6aae9d1fd5ca9ecd07e195
Jindrg <jindrg@gmail.com>
2022-09-28 09:56:00 -0300

    Incluyendo un test

--- test/productTest.js
+++ test/productTest.js
@@ -2,50 +2,48 @@ const chai = require('chai');
 const assert = chai.assert;
 const expect = chai.expect
 
-let Product = require('../src/models/Product.js')
-
-let ProductSuc = require('../src/models/ProductSucursal')
-const prodSuc = new ProductSuc({ idProd: 3, barCode: 111, nomCat: "Frutas", marca: "Ecuador", description: "Banana", stock: 100, idSuc: 2, salePrice: 155 })
+let Product = require('../src/models/Product.js');
+let ProductSucursal = require('../src/models/ProductSucursal.js')
+const prodSuc = new ProductSucursal({ idProd: 3, barCode: 111, nomCat: "Frutas", marca: "Ecuador", description: "Banana", stock: 100, idSuc: 2, salePrice: 155 })
 
 describe("ProductoDeSucursal", () => {
     describe("Registros", () => {
-        it('debe tener los siguientes datos', () => {
+        it('debe tener cargado un registro en ID', () => {
             assert.equal(prodSuc.idProd, 3)
         })
 
-        it('debe tener cargado un atributo BARCODE', () => {
+        it('debe tener cargado un registro en BARCODE', () => {
             assert.equal(prodSuc.barCode, 111)
         })
 
-        it('debe tener cargado un atributo NOMBRE_DE_CATEGORIA', () => {
+        it('debe tener cargado un registro en NOMBRE_DE_CATEGORIA', () => {
             assert.equal(prodSuc.nomCat, 'Frutas')
         })
 
-        it('debe tener cargado un atributo MARCA', () => {
+        it('debe tener cargado un registro en MARCA', () => {
             assert.equal(prodSuc.marca, 'Ecuador')
         })
 
-        it('debe tener cargado un atributo DESCRIPCION', () => {
+        it('debe tener cargado un registro en DESCRIPCION', () => {
             assert.equal(prodSuc.description, 'Banana')
         })
 
-        it('debe tener cargado un atributo STOCK', () => {
+        it('debe tener cargado un registro en STOCK', () => {
             assert.equal(prodSuc.stock, 100)
         })
 
-        it('debe tener cargado un atributo ID_SUC', () => {
+        it('debe tener cargado un registro en ID_SUC', () => {
             assert.equal(prodSuc.idSuc, 2)
         })
 
-        it('debe tener cargado un atributo PRECIO_DE_LISTA', () => {
+        it('debe tener cargado un registro en PRECIO_DE_LISTA', () => {
             assert.equal(prodSuc.salePrice, 155)
         })
     })
 })
 
-
-const prod = new Product(3,111,"Frutas","Ecuador","Banana",100, 2,155)
-const atributosDelObjetoProduct = Object.keys(prod)
+const prodSu = new Product(3, 111, "Frutas", "Ecuador", "Banana", 100, 2, 155)
+const atributosDelObjetoProduct = Object.keys(prodSu)
 describe("Product", () => {
     describe("atributos", () => {
         it('debe tener un atributo ID', () => {
@@ -72,4 +70,16 @@ describe("Product", () => {
             assert.equal(atributosDelObjetoProduct[5], 'stock')
         })
     })
-})
+}) 
+
+
+describe("Product", function() {
+    describe("Stock", function() {
+    it("Stock no tiene que ser negativo ", function() {
+    result   = prodSu.validateStatusStock(1)
+    console.log(result)
+   expect([0,1]).to.include(result) 
+    
+    }); 
+    });
+   });
