fedd057fc14d910a58202891068dd8004bf14d5b
Nicolaiev Brito Pustelnik <nicolaievbrito@gmail.com>
2022-09-27 22:13:47 -0300

    add tests

--- test/adminTest.js
+++ test/adminTest.js
@@ -4,6 +4,7 @@ const assert = chai.assert;
 const expect = chai.expect;
 
 const Admin = require('../src/models/Admin');
+const Employee = require('../src/models/Employee');
 const Sucursal = require('../src/models/Sucursal');
 
 describe("Admin", function () {
@@ -23,36 +24,86 @@ describe("Admin", function () {
         }));
     });
 
-    // describe("Method delete user", function () {
+    describe("Method delete user", function () {
 
-    //     let newAdmin = new Admin({ firstName: "Nicolaiev", lastName: "Brito", email: "nicolaievbrito@gmail.com", password: "12345", sucursal: "1" });
+        let newAdmin = new Admin({ firstName: "Nicolaiev1", lastName: "Brito1", email: "nicolaievbrito1@gmail.com", password: "12345", sucursal: "1" });
+        let Suc = new Sucursal({ idSuc: 1, nomSuc: "suc1", ubicacion: "Belgrano" });
 
-    //     let Suc = new Sucursal({ idSuc: 1, nomSuc: "suc1", ubicacion: "Belgrano" });
+        let userDeleted = newAdmin.deleteUser(Suc, 1);
 
-    //     //let Suc = new Object;
+        it("Delete user", (function () {
+            assert.equal(userDeleted, true);
+        }));
 
-    //     let userDeleted = newAdmin.deleteUser(Suc, 1);
+        it("The result is a boolean", (function () {
+            assert.typeOf(userDeleted, "boolean");
+        }));
 
-    //     it("Delete user", (function () {
-    //         assert.equal(userDeleted, true);
-    //     }));
+        it("Assert catch exception empty Suc", (function () {
+            assert.throws(() => newAdmin.deleteUser({}, 1), Error, "Local invalido");
+        }));
 
-    //     it("The result is a boolean", (function () {
-    //         assert.typeOf(userDeleted, "boolean");
-    //     }));
+        it("Assert catch exception null Suc", (function () {
+            assert.throws(() => newAdmin.deleteUser(null, 1), Error, "Local invalido");
+        }));
 
-    //     // it("Assert catch exception null Suc", (function () {
-    //     //     assert.throws(() => newAdmin.deleteUser({}, 1), Error, "Local invalido");
-    //     // }));
+        it("Assert catch exception invalid number idUser", (function () {
+            assert.throws(() => newAdmin.deleteUser(Suc, 0), Error, "IdUser invalido");
+        }));
 
-    //     it("Assert catch exception invalid number idUser", (function () {
-    //         assert.throws(() => newAdmin.deleteUser(Suc, 0), Error, "IdUser invalido");
-    //     }));
+        it("Assert catch exception null idUser", (function () {
+            assert.throws(() => newAdmin.deleteUser(Suc, null), Error, "IdUser invalido");
+        }));
+    });
 
-    //     it("Assert catch exception null idUser", (function () {
-    //         assert.throws(() => newAdmin.deleteUser(Suc, null), Error, "IdUser invalido");
-    //     }));
-    // });
+    describe("Method create user", function () {
+
+        let Suc = new Sucursal({ idSuc: 1, nomSuc: "suc1", ubicacion: "Belgrano" });
+        let newAdmin = new Admin({ firstName: "Nicolaiev2", lastName: "Brito2", email: "nicolaievbrito2@gmail.com", password: "54321", sucursal: "2" });
+        let newEmployee = new Employee({ firstName: "Nicolaiev2", lastName: "Brito2", email: "nicolaievbrito2@gmail.com", password: "54321", sucursal: "2" });
+
+        let createAdmin = newAdmin.createUser(Suc, newAdmin);
+        let createEmployee = newAdmin.createUser(Suc, newEmployee);
+
+        it("Assert catch exception empty Suc", (function () {
+            assert.throws(() => newAdmin.createUser({}, 1), Error, "Local invalido");
+        }));
+
+        it("Assert catch exception null Suc", (function () {
+            assert.throws(() => newAdmin.createUser(null, 1), Error, "Local invalido");
+        }));
+
+        it("Assert catch exception invalid User", (function () {
+            assert.throws(() => newAdmin.createUser(Suc, {}), Error, "User invalido");
+        }));
+
+        it("Assert catch exception null User", (function () {
+            assert.throws(() => newAdmin.createUser(Suc, null), Error, "User invalido");
+        }));
+
+        describe("User Admin", function () {
+
+            it("Create Admin", (function () {
+                assert.equal(createAdmin, true);
+            }));
+
+            it("The result is a boolean Admin", (function () {
+                assert.typeOf(createAdmin, "boolean");
+            }));
+        });
+
+        describe("User Employee", function () {
+
+            it("Create Employee", (function () {
+                assert.equal(createEmployee, true);
+            }));
+
+            it("The result is a boolean Employee", (function () {
+                assert.typeOf(createEmployee, "boolean");
+            }));
+        });
+
+    });
 
 });
 
