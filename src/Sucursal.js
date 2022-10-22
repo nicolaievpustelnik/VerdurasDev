1733cf1c38db2ade141663ca0adae8ecdb05a624
Nicolaiev Brito Pustelnik <nicolaievbrito@gmail.com>
2022-10-22 12:37:46 -0300

    Modificaciones logica

--- src/Sucursal.js
+++ src/Sucursal.js
@@ -15,12 +15,60 @@ class Sucursal {
         this.productos = [];
     }
 
+    recepcionarProducto(idProveedor, idProducto, cant) {
+        let unEmpleado = this.obtenerUsuarioLogueado()
+        if (unEmpleado != null) {
+            if (unEmpleado.getRol() == Rol.RECEPCIONISTA || unEmpleado.getRol() == Rol.ORGANIZADOR) {
+                let unProveedor = this.obtenerProveedor(idProveedor)
+                if (unProveedor != null) {
+                    let unProductoSucursal = this.buscarProductoEnSucursal(idProducto)
+                    if (unProductoSucursal != null) {
+                        if (this.cantidadEsValida(cant)) {
+                            unProductoSucursal.actualizarStock(cant)
+                            this.movimientos.push(new Movimiento(this.calcularMonto(cant, this.obtenerPrecioCompra(idProd)),
+                                this.generarIdMovimiento(), this.generarFechaYhoraActual(), idProveedor))
+                        } else {
+                            this.dispararAlerta(unEmpleado, "Intento de ingresar cantidad fuera de parametros")
+                        }
+                    } else {
+                        console.log("Producto no esta en surtido, agregue producto antes de recepcionar!")
+                    }
+                } else {
+                    console.log("Proveedor no autorizado para entrega")
+                    this.dispararAlerta(unEmpleado, "Intenta dar ingreso de mercaderia no autorizada")
+                }
+            } else {
+                console.log("No cumple con el rol correspondiente!")
+                this.dispararAlerta(unEmpleado, "Intenta ejecutar una tarea no autorizada")
+            }
+        } else {
+            console.log("Empleado no Existe!")
+        }
+    }
+
+    obtenerStockProducto(idProducto) {
+        return 0
+    }
+
+    buscarProductoEnSucursal(idProdSuc) {
+        return productosSucursal.find(ps => ps.idProducto == idProdSuc);
+    }
+
+    buscarEmpleado(idEmpleado) {
+        return this.empleados.find(u => u.idEmpleado == idEmpleado);
+    }
+
     agregarProducto(productAux) {
         let p = new ProductoSucursal(productAux)
         this.productos.push(p)
     }
 
     agregarUsuario(userAux) {
+        let e = new Empleado(userAux)
+        this.usuarios.push(e)
+    }
+
+    recibirProductoStock(userAux) {
         let e = new Empleado(userAux)
         this.usuarios.push(e)
     }
