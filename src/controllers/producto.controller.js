9c09311f841e497a2420626931b536a34422f2ec
Jindrg <jindrg@gmail.com>
2022-11-11 12:58:23 -0300

    Agregando Route, Controlador, vista de Producto(incompleto)

--- src/controllers/producto.controller.js
+++ src/controllers/producto.controller.js
@@ -1,36 +1,42 @@
 const ProductoSucursal = require('../models/ProductoSucursal');
+const ProductoProveedor = require('../models/ProductoProveedor');
+const Producto = require('../models/Producto');
+
+const productosController = {};
 
 // Nuevo Prodcuto
-usuariosControllers.renderizarFormUsuario = (req, res) => {
-    res.render('usuario/nuevoUsuario');
+productosController.renderizarFormProducto = (req, res) => {
+    res.render('producto/nuevoProducto');
 }
 
-usuariosControllers.crearUsuario = async (req, res) => {
+productosControllers.crearProducto = async (req, res) => {
     try {
 
-        const { nombre, apellido, email, password, sucursal, tipoUsuario, rol } = req.body;
+        const { idProducto, codigoBarra, nombreCategoria, marca, descripcion, stock,idSucursal,idProveedor,precioVenta,precioCompra,tipoProducto } = req.body;
 
-        let newUser = null;
 
-        switch (tipoUsuario) {
-            case 'Admin':
-                newUser = new Admin({ nombre, apellido, email, password, sucursal, tipoUsuario });
+
+        let nuevoProducto = null;
+
+        switch (tipoProducto) {
+            case 'Producto':
+                nuevoProducto = new ProductoSucursal({idProducto, codigoBarra, nombreCategoria, marca, descripcion, stock, idSucursal, precioVenta});
                 break;
 
-            case 'Empleado':
-                newUser = new Empleado({ nombre, apellido, email, password, sucursal, tipoUsuario, rol });
+            case 'Proveedor':
+                nuevoProducto = new ProductoProveedor({ codigoBarra, nombreCategoria, marca, descripcion, stock,idProveedor, precioCompra});
                 break;
 
             default:
                 break;
         }
 
-        await newUser.save();
+        await nuevoProducto.save();
 
-        res.send('Usuario agregado');
+        res.send('Producto agregado');
 
-    } catch (e) {
+    } catch (err) {
 
-        console.log(e)
+        console.log(err.name +" --> "+err.message)
     }
 }
